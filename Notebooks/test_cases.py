54769e7b639fe1f9aef265c472ef8a36838485fe
carlosaol <carlos_aol@hotmail.com>
2025-04-11 15:11:55 -0600

    add test_cases in a separated file

---
+++ Notebooks/test_cases.py
@@ -0,0 +1,204 @@
+import unittest
+import numpy as np
+from IPython.display import Markdown, display
+
+import __main__
+
+global test_func
+
+class test_init_layer(unittest.TestCase):
+    
+    def test_return(self):
+        global test_func
+        ret = test_func(2,3)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+        
+    def test_shape(self):
+        global test_func
+        l1=np.random.randint(10)
+        l2=np.random.randint(10)
+        w,b= test_func(l1,l2)
+        self.assertEqual(w.shape,(l2,l1),msg ='the weights shape is incorrect')
+        self.assertEqual(b.shape,(l2,1),msg ='the weights shape is incorrect')
+
+    def test_distribution(self):
+        global test_func
+        w,b=test_func(4,6)
+        self.assertLessEqual(abs(w.mean()),1 , msg = "mean of the weights should be less than one")
+        self.assertLessEqual(abs(b.mean()),1 , msg =  "mean  of the bias should be less or equal than one")
+
+        self.assertNotEqual(w.std(),0 , msg = "all your vales are equal")
+        self.assertNotEqual(b.std(),0 , msg = "all your vales are equal")
+
+class test_preactivation(unittest.TestCase):
+    #global variables
+    def get_random_weights(self):
+        l1=np.random.randint(3,10)
+        l2=np.random.randint(3,10)
+        w= np.random.randint(10,size=(l2,l1))
+        b= np.random.randint(10,size=(l2,1))
+        x= np.random.randint(10,size=(l1,1))
+
+        return w,b,x
+    
+    def test_return(self):
+        global test_func
+        w,b,x = self.get_random_weights()
+        ret = test_func(w,b,x)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+
+    def test_shape(self):
+        global test_func
+        w,b,x = self.get_random_weights()
+        z = test_func(w,b,x)
+        self.assertEqual(z.shape,(len(b),1),msg ='the activation output shape is incorrect, review the matrix order')
+
+    def test_known_values(self):
+        global test_func
+        w = np.array([[2,4],[3,5]])
+        b = np.array([[0],[-6]])
+        x= np.array([[2],[4]])
+        z = test_func(w,b,x)
+        expected =np.array([[20],[20]])
+        is_equal = np.equal(z,expected).all()
+        self.assertEqual(is_equal,True, msg ='result is not correct, review the equations')
+
+    def test_random_values(self):
+        global test_func
+        w,b,x = self.get_random_weights()
+        z = test_func(w,b,x)
+        expected = np.dot(w,x)+b
+        is_close = np.isclose(z,expected).all()
+        self.assertEqual(is_close,True, msg ='result is not correct, review the equations')
+
+
+class test_relu(unittest.TestCase): 
+    global test_func
+    def test_return(self):
+        ret = test_func(10)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+        
+    def test_positive(self):
+        ret = test_func(10)
+        self.assertEqual(ret,10, msg = 'the output is incorrect for positive values')
+        eps = 0.0001
+        ret = test_func(eps)
+        self.assertEqual(ret,eps, msg = 'the output is incorrect for small positive values')
+        x = np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,x, msg = 'the output is incorrect for large positive values')
+
+    def test_negative(self):
+        ret = test_func(-10)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for negative values')
+        eps = -0.0001
+        ret = test_func(eps)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for small negative values')
+        x = -np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for large negative values')
+
+class test_relu_prime(unittest.TestCase):  
+    global test_func
+    def test_return(self):
+        ret = test_func(10)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+        
+    def test_positive(self):
+        ret = test_func(10)
+        self.assertEqual(ret,1, msg = 'the output is incorrect for positive values')
+        eps = 0.0001
+        ret = test_func(eps)
+        self.assertEqual(ret,1, msg = 'the output is incorrect for small positive values')
+        x = np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,1, msg = 'the output is incorrect for large positive values')
+
+    def test_negative(self):
+        ret = test_func(-10)
+        self.assertEqual(ret,0, msg = 'the output is incorrect')
+        eps = -0.0001
+        ret = test_func(eps)
+        self.assertEqual(ret,0, msg = 'the output is incorrect')
+        x = -np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for large negative values')
+
+class test_sigmoid(unittest.TestCase): 
+    def test_return(self):
+        global test_func
+        ret = test_func(10)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+        
+    def test_positive(self):
+        global test_func
+        ret = test_func(0)
+        self.assertEqual(ret,0.5, msg = 'the output is incorrect')
+        ret = test_func(np.log(2))
+        self.assertAlmostEqual(ret,2/3, places = 4, msg= 'the output is incorrect for  positive values')
+        x = np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,1, msg = 'the output is incorrect for large positive values')
+
+    def test_negative(self):
+        global test_func
+        ret = test_func(-np.log(2))
+        self.assertAlmostEqual(ret,1/3, places=4, msg= 'the output is incorrect for negative values')
+        ret = test_func(-np.log(4))
+        self.assertAlmostEqual(ret,1/5, places=4, msg= 'the output is incorrect for negative values')
+        x = -np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for large negative values')
+
+class test_sigmoid_prime(unittest.TestCase):   
+    def test_return(self):
+        global test_func
+        ret =  test_func(10)
+        self.assertIsNotNone(ret,msg = "fuction doesn't return anything")
+        
+    def test_positive(self):
+        global test_func
+        ret =  test_func(0)
+        self.assertEqual(ret,0.25, msg = 'the output is incorrect for positive values')
+        ret =  test_func(np.log(2))
+        self.assertAlmostEqual(ret,2/9, places=4, msg= 'the output is incorrect for positive values')
+        x = np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for large positive values')
+
+    def test_negative(self):
+        global test_func
+        ret =  test_func(-np.log(2))
+        self.assertAlmostEqual(ret,2/9 , places=4, msg= 'the output is incorrect for negative values')
+        ret =  test_func(-np.log(4))
+        self.assertAlmostEqual(ret,4/25, places=4, msg= 'the output is incorrect for negative values')
+        x = -np.inf
+        ret = test_func(x)
+        self.assertEqual(ret,0, msg = 'the output is incorrect for large negative values')
+
+Tests={ 'test_init_layer'    : test_init_layer,
+        'test_preactivation' : test_preactivation,
+        'test_relu'          : test_relu,
+        'test_relu_prime'    : test_relu_prime,
+        'test_sigmoid'       : test_sigmoid,
+        'test_sigmoid_prime' : test_sigmoid_prime}
+
+def printmd(string):
+    display(Markdown(string))
+    #suite = unittest.TestSuite()
+    #suite.addTest(test_init_layer(func))
+    #unittest.TextTestRunner(verbosity=2).run(suite)
+def run_check(check_name, func):
+    global test_func
+
+    check = Tests[check_name]
+    test_func = func
+    results = unittest.main(module=check(),argv=[''], verbosity=2, exit=False)
+
+    if(results.result.wasSuccessful()):
+        printmd('**<span style="color: green;">PASSED</span>**')
+    else:
+        printmd('**<span style="color: red;">FAILED</span>**')
+
+    return results.result
+    
